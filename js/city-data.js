b2b6f4bd191a7bc3dde7c9bba5712f2c2261d9d7
Ashley Zufelt <ashley.zufelt@gmail.com>
2021-09-10 11:32:30 -0400

    data sources, etc

--- js/city-data.js
+++
@@ -1,9 +0,0 @@
-const APPID ="5d7ca0ee2552ad81834842d1a61b5052cee12272";
-const apiURL = "https://api.census.gov/data/2020/dec/responserate?get=NAME,GEO_ID,DRRALL,CRRINT,RESP_DATE,CRRALL,DRRINT&for=county:003&in=state:12&key=${APPID}";
-
-
-fetch(apiURL) 
-  .then((response) => response.json())
-  .then((jsObject) => {
-    console.log(jsObject);
-  })
